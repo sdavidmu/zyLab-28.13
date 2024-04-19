2978b6861fb55a47961f1b324d54882500036881
david <dzachariah@smu.edu>
2024-04-19 14:56:41 -0500

    Iswear

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-// ny new welcome message from VS code in a second branch IT SHOYLD WORK THIS TIME
+// ny new welcome message from VS code in a second branch IT SHOULD WORK THIS TIME
 
 // update from user 1jhkb vguv
 int main () {
