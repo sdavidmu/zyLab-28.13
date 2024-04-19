e775d13ff5e8414a7fe7cac14f4a457e32ecd698
david <dzachariah@smu.edu>
2024-04-19 16:42:37 -0500

    finished I think

--- ItemToPurchase.cpp
+++ ItemToPurchase.cpp
@@ -1,1 +1,35 @@
-// stuff
+#include <iostream>
+using namespace std;
+#include "ItemToPurchase.h"
+
+// ItemToPurchase constructor
+ItemToPurchase::ItemToPurchase(string itemName="none", int itemPrice=0, int itemQuantity=0) {   
+    this->itemName = itemName;
+    this->itemPrice = itemPrice;
+    this->itemQuantity = itemQuantity;
+}
+
+void ItemToPurchase::SetName(string name) {
+    this->itemName = name;
+}
+
+string ItemToPurchase::GetName() {
+    return itemName;
+}
+
+void ItemToPurchase::SetPrice(int price) {
+    this->itemPrice = price;
+}
+
+int ItemToPurchase::GetPrice() {
+    return itemPrice;
+}
+
+void ItemToPurchase::SetQuantity(int quantity) {
+    this->itemQuantity = quantity;
+}
+
+int ItemToPurchase::GetQuantity() {
+    return itemQuantity;
+}
+
--- main.cpp
+++ main.cpp
@@ -1,10 +1,48 @@
 #include <iostream>
+#include "ItemToPurchase.h"
 using namespace std;
 
-// ny new welcome message from VS code in a second branch IT SHOULD WORK THIS TIME I SWEAR NOW IT WILL WORK
-
-// update from user 1jhkb vguv
 int main () {
+
+  ItemToPurchase item;
+  ItemToPurchase item2;
+
+  string item1Name;
+  int item1Price;
+  int item1Quantity;
+  string item2Name;
+  int item2Price;
+  int item2Quantity;
+
+  cout << "Item 1" << endl;
+  cout << "Enter the item name: " << endl;
+  getline(cin, item1Name);
+  item.SetName(item1Name);
+  cout << "Enter the item price: " << endl;
+  cin >> item1Price;
+  item.SetPrice(item1Price);
+  cout << "Enter the item quantity: " << endl;
+  cin >> item1Quantity;
+  item.SetQuantity(item1Quantity);
+
+  cin.ignore();
+
+  cout << "Item 2" << endl;
+  cout << "Enter the item name: " << endl;
+  getline(cin, item2Name);
+  item2.SetName(item2Name);
+  cout << "Enter the item price: " << endl;
+  cin >> item2Price;
+  item2.SetPrice(item2Price);
+  cout << "Enter the item quantity: " << endl;
+  cin >> item2Quantity;
+  item2.SetQuantity(item2Quantity);
+  
+  cout << "TOTAL COST" << endl;
+  cout << item.GetName() << " " << item.GetQuantity() << "@ $" << item.GetPrice() << " = $" << item.GetPrice() * item.GetQuantity() << endl; 
+  cout << item2.GetName() << " " << item2.GetQuantity() << "@ $" << item2.GetPrice() << " = $" << item2.GetPrice() * item2.GetQuantity() << endl; 
+  cout << endl;
+  cout << "Total: $" << (item.GetPrice() * item.GetQuantity() + item2.GetPrice() * item2.GetQuantity()) << endl;
 
   return 0;
 }
